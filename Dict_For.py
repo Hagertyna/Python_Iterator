511204606353879d48944d8bfc4518e7293ce119
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-17 05:25:25 -0700

    Create Dict_For.py

---
+++ Dict_For.py
@@ -0,0 +1,9 @@
+fcc_dict = {"name": "TechCampEthiopia",
+           "type": "non-profit", 
+           "mode": "remote", 
+           "paid": "no"}
+
+for key in fcc_dict:
+    print(key, end=" ")
+
+# Output: name type mode paid 
