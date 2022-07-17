e434859231b857cf6a5152557fee1366efafe453
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-17 05:43:06 -0700

    Update For_dictionary.py

--- For_dictionary.py
+++ For_dictionary.py
@@ -11,3 +11,14 @@ for key, value in fcc_dict.items():
 # type non-profit
 # mode remote
 # paid no
+fcc_dict = {"name": "TechCampEthiopia",
+           "type": "non-profit", 
+           "mode": "remote", 
+           "paid": "no"}
+
+for a, b in fcc_dict.items():
+    # print(a, b)
+    if a == "type":
+        print("TechCampEthiopia is a non-profit organization")
+
+# Output: TechCampEthiopia is a non-profit organization
