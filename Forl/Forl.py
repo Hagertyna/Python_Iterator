91d0358a4e2569a41083c9907bb2d47937e0921d
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-16 23:00:03 -0700

    Update Forl.py

--- Forl/Forl.py
+++ Forl/Forl.py
@@ -3,3 +3,7 @@ for even in Even numbers:
   print(even , "Is an Even number")
 for num in Even numbers:
   print(num,End=" ")
+Oddnum = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+
+for odnum in Oddnum:
+  print(odnum, "Is an Odd number")
