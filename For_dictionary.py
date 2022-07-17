86acadcdd3481f4819a6e497799f571362a54487
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-17 05:30:33 -0700

    Create For_dictionary.py

---
+++ For_dictionary.py
@@ -0,0 +1,13 @@
+fcc_dict = {"name": "TechCampEthiopia",
+           "type": "non-profit", 
+           "mode": "remote", 
+           "paid": "no"}
+
+for key, value in fcc_dict.items():
+    print(key, value)
+
+# Output: 
+# name TechCampEthiopia
+# type non-profit
+# mode remote
+# paid no
