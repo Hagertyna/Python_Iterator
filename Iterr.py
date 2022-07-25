6599d66237dac0df42b3db45704f117a53e3bc83
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-24 22:54:29 -0700

    Update Iterr.py

--- Iterr.py
+++ Iterr.py
@@ -1,6 +1,8 @@
  a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
  for key in a_dict:
    print(key)
+   
+#Sorted by key
 incomes = {'apple': 5600.00, 'orange': 3500.00, 'banana': 5000.00}
 for key in sorted(incomes):
     print(key, '->', incomes[key])
