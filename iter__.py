9bdf89f4a985099ea3150821193b1dae78de76d9
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-26 23:44:32 -0700

    Update iter__.py

--- iter__.py
+++ iter__.py
@@ -25,3 +25,15 @@ next(my_iter)
 
 for element in my_list:
     print(element)
+# create an iterator object from that iterable
+iter_obj = iter(iterable)
+
+# infinite loop
+while True:
+    try:
+        # get the next item
+        element = next(iter_obj)
+        # do something with element
+    except StopIteration:
+        # if StopIteration is raised, break from loop
+        break
